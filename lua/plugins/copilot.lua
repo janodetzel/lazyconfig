386c16e45b328e48b14e4a1ca59ecb4e530318a5
Jano Detzel <jano.detzel@ewe-go.de>
2026-02-20 14:21:12 +0100

    Exclude .env for copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,3 +1,15 @@
 return {
   "github/copilot.vim",
+  init = function()
+    local group = vim.api.nvim_create_augroup("copilot_disable_env_files", { clear = true })
+    vim.api.nvim_create_autocmd({ "BufEnter", "BufNewFile" }, {
+      group = group,
+      callback = function(args)
+        local name = vim.fn.fnamemodify(args.file, ":t")
+        if name:match("^%.env") then
+          vim.b[args.buf].copilot_enabled = false
+        end
+      end,
+    })
+  end,
 }
